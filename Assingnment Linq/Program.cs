81d834ba42e4471f6727dce445f4e1b08243e786
SI01029 <SaeedIbrahim01029@gmail.com>
2025-11-05 00:27:22 +0200

    Add project files.

---
+++ Assingnment Linq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assingnment_Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
